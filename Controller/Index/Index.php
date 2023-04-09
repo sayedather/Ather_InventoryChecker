8bc9302121357818283b1dfb303c2c1f98c79d36
Ather Akber <ather90@gmail.com>
2023-04-09 09:31:04 +0300

    Create Index.php

---
+++ Controller/Index/Index.php
@@ -0,0 +1,24 @@
+<?php
+namespace InventoryChecker\Controller\Index;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
