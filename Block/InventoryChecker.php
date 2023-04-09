53164a3a6aafd5d9284731a72dda2e273549818c
Ather Akber <ather90@gmail.com>
2023-04-09 09:44:07 +0300

    Update the module to enable/disable sku,gtin,images or the module itself

--- Block/InventoryChecker.php
+++ Block/InventoryChecker.php
@@ -4,20 +4,24 @@ namespace InventoryChecker\Block;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\CatalogInventory\Api\StockRegistryInterface;
 use Magento\Catalog\Model\ProductRepository;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class InventoryChecker extends \Magento\Framework\View\Element\Template
 {
     protected $stockRegistry;
     protected $productRepository;
+    protected $scopeConfig;
 
     public function __construct(
         Context $context,
         StockRegistryInterface $stockRegistry,
         ProductRepository $productRepository,
+        ScopeConfigInterface $scopeConfig,
         array $data = []
     ) {
         $this->stockRegistry = $stockRegistry;
         $this->productRepository = $productRepository;
+        $this->scopeConfig = $scopeConfig;
         parent::__construct($context, $data);
     }
 
@@ -39,5 +43,25 @@ class InventoryChecker extends \Magento\Framework\View\Element\Template
             $product = $this->productRepository->getByAttribute('ean', $gtin);
         }
         return $product;
+    }
+
+    public function getImageSize()
+    {
+        return $this->scopeConfig->getValue('inventorychecker_settings/general/image_size');
+    }
+
+    public function isShowSku()
+    {
+        return $this->scopeConfig->getValue('inventorychecker_settings/general/show_sku');
+    }
+
+    public function isShowGtin()
+    {
+        return $this->scopeConfig->getValue('inventorychecker_settings/general/show_gtin');
+    }
+
+    public function isShowImage()
+    {
+        return $this->scopeConfig->getValue('inventorychecker_settings/general/show_image');
     }
 }
